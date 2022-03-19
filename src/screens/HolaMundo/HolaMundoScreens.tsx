23d65efde4cd496ce212f6d383badc69de19b933
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2022-03-19 17:42:06 -0300

    refactor HolaMundo

---
+++ src/screens/HolaMundo/HolaMundoScreens.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+export const HolaMundoScreens = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Screen Hola Mundo</Text>
+    </View>
+  );
+};
---
+++ src/screens/HolaMundo/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 45,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
